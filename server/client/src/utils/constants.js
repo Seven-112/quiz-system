2f9fb8e605d70e691192442f813b4bc2aa24585f
Mouthwatering777 <bluesatellite777@gmail.com>
2023-01-13 21:36:21 +0200

    ip

--- server/client/src/utils/constants.js
+++ server/client/src/utils/constants.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000'
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://63.250.42.167:5000'
 // const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://64.44.141.19:8080'
 const CHEATNUM = 16
 export {
